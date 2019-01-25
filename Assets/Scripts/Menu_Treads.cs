6a6ebff52541114802a13e0d2271cf9f836b17d3
Agentkow <kennethkhau212@gmail.com>
2019-01-25 16:33:31 -0600

    Main Menu Change

---
+++ Assets/Scripts/Menu_Treads.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Treads : MonoBehaviour
+{
+    private Animator animate;
+
+    void Start()
+    {
+        animate = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animate.SetBool("Treads", true);
+    }
+}
